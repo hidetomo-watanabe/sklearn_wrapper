4605cc76a9d5ca13bc75255c672e88c6d8cf17cf
hidetomo-watanabe <attyamunn@gmail.com>
2018-10-24 00:19:16 +0000

    plasticcの変数変換追加

--- myfuncs/plasticc.py
+++ myfuncs/plasticc.py
@@ -9,5 +9,15 @@ def translate_mjd(dfs, train_df):
     return dfs
 
 
+def translate_flux(dfs, train_df):
+    #######################################
+    # flux_diff = flux_max - flux_min
+    #######################################
+    for df in dfs:
+        df['flux_diff'] = df['flux_max'] - df['flux_min']
+        df['flux_diff2'] = df['flux_diff'] / df['flux_mean']
+    return dfs
+
+
 if __name__ == '__main__':
     pass
