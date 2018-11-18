38002858f30c37f689de36a18cca17044c24f26b
hidetomo-watanabe <attyamunn@gmail.com>
2018-11-18 08:24:30 +0000

    flake8のpass修正

--- tests/check_with_flake8.sh
+++ tests/check_with_flake8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-flake8_msg=$(flake8 modules/*.py --ignore E402 | grep -v myfunc)
-flake8_msg=${flake8_msg}$(flake8 myfuncs/*.py --ignore E402)
-flake8_msg=${flake8_msg}$(flake8 scripts/*.py --ignore E402)
+flake8_msg=$(flake8 modules/ --ignore E402 | grep -v myfunc)
+flake8_msg=${flake8_msg}$(flake8 myfuncs/ --ignore E402)
+flake8_msg=${flake8_msg}$(flake8 scripts/ --ignore E402)
 if [ -n "${flake8_msg}" ];then
   echo 'FLAKE8 TEST ERROR'
   echo -e "${flake8_msg}"
