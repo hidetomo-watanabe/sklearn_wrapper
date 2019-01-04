2e5de7a6281e2e90e1299dc687963a3303275b8a
hidetomo-watanabe <attyamunn@gmail.com>
2019-01-04 00:42:00 +0000

    titanic2のtest追加

--- tests/check_predict.sh
+++ tests/check_predict.sh
@@ -9,18 +9,22 @@ catch()
   fi
 }
 
-if [ -e 'outputs/tmp_titanic.csv' ]; then
-  rm outputs/tmp_titanic.csv
-fi
-if [ -e 'outputs/proba_tmp_titanic.csv' ]; then
-  rm outputs/proba_tmp_titanic.csv
-fi
-if [ -e 'outputs/tmp_house.csv' ]; then
-  rm outputs/tmp_house.csv
-fi
+_del()
+{
+  if [ -e $1 ]; then
+    rm $1
+  fi
+}
+
+_del 'outputs/tmp_titanic.csv'
+_del 'outputs/tmp_titanic2.csv'
+_del 'outputs/proba_tmp_titanic.csv'
+_del 'outputs/proba_tmp_titanic2.csv'
+_del 'outputs/tmp_house.csv'
 
 # titanic
 err_msg=$(python -u scripts/predict.py tests/titanic/test_config.json | grep ERROR | :)
+err_msg=${err_msg}$(python -u scripts/predict.py tests/titanic/test_config2.json | grep ERROR | :)
 err_msg=${err_msg}$(python -u scripts/predict.py tests/house/test_config.json | grep ERROR | :)
 if [ -n "${err_msg}" ];then
   echo 'PREDICT TEST ERROR'
@@ -29,6 +33,7 @@ if [ -n "${err_msg}" ];then
 fi
 
 diff_msg=$(diff outputs/tmp_titanic.csv tests/titanic/output.csv)
+diff_msg=${diff_msg}$(diff outputs/tmp_titanic2.csv tests/titanic/output2.csv)
 diff_msg=${diff_msg}$(diff outputs/tmp_house.csv tests/house/output.csv)
 if [ -n "${diff_msg}" ];then
   echo 'PREDICT TEST ERROR'
@@ -37,6 +42,7 @@ if [ -n "${diff_msg}" ];then
 fi
 
 diff_msg=$(diff outputs/proba_tmp_titanic.csv tests/titanic/proba_output.csv)
+diff_msg=${diff_msg}$(diff outputs/proba_tmp_titanic2.csv tests/titanic/proba_output2.csv)
 if [ -n "${diff_msg}" ];then
   echo 'PREDICT TEST ERROR'
   echo -e "${diff_msg}"
