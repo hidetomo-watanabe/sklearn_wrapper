b6fea5cb13f8d5201c26e3d5a47cad3e138cd830
hidetomo-watanabe <attyamunn@gmail.com>
2019-01-27 02:03:31 +0000

    kernelでエラー補足やめる

--- predict_for_kernel_base.py
+++ predict_for_kernel_base.py
@@ -6,45 +6,41 @@ logger = MyLogger().get_logger('predict')
 if __name__ == '__main__':
     logger.info('# START')
 
-    try:
-        predicter_obj = Predicter(kernel=True)
-        predicter_obj.read_config_text(config_text)
+    predicter_obj = Predicter(kernel=True)
+    predicter_obj.read_config_text(config_text)
 
-        logger.info('### INIT')
-        predicter_obj.get_raw_data()
-        predicter_obj.display_data()
+    logger.info('### INIT')
+    predicter_obj.get_raw_data()
+    predicter_obj.display_data()
 
-        logger.info('### TRANSLATE')
-        predicter_obj.trans_raw_data()
-        predicter_obj.display_data()
+    logger.info('### TRANSLATE')
+    predicter_obj.trans_raw_data()
+    predicter_obj.display_data()
 
-        logger.info('##### NORMALIZE')
-        predicter_obj.get_fitting_data()
-        predicter_obj.normalize_fitting_data()
-        predicter_obj.reduce_dimension()
+    logger.info('##### NORMALIZE')
+    predicter_obj.get_fitting_data()
+    predicter_obj.normalize_fitting_data()
+    predicter_obj.reduce_dimension()
 
-        # logger.info('### VISUALIZE TRAIN DATA')
-        # predicter_obj.visualize_train_data()
+    # logger.info('### VISUALIZE TRAIN DATA')
+    # predicter_obj.visualize_train_data()
 
-        logger.info('### VALIDATE')
-        predicter_obj.is_ok_with_adversarial_validation()
+    logger.info('### VALIDATE')
+    predicter_obj.is_ok_with_adversarial_validation()
 
-        logger.info('### FIT')
-        predicter_obj.calc_ensemble_model()
+    logger.info('### FIT')
+    predicter_obj.calc_ensemble_model()
 
-        # logger.info('### VISUALIZE LEARNING CURVES')
-        # predicter_obj.visualize_learning_curves()
+    # logger.info('### VISUALIZE LEARNING CURVES')
+    # predicter_obj.visualize_learning_curves()
 
-        logger.info('### PREDICT')
-        predicter_obj.calc_output()
+    logger.info('### PREDICT')
+    predicter_obj.calc_output()
 
-        # logger.info('### VISUALIZE TRAIN PRED DATA')
-        # predicter_obj.visualize_train_pred_data()
+    # logger.info('### VISUALIZE TRAIN PRED DATA')
+    # predicter_obj.visualize_train_pred_data()
 
-        logger.info('### OUTPUT')
-        predicter_obj.write_output()
+    logger.info('### OUTPUT')
+    predicter_obj.write_output()
 
-    except Exception as e:
-        logger.error('%s' % e)
-        traceback.print_exc()
     logger.info('# FINISHED')
