354ae1c0485efacf5c15d1577d8770f08bdcf551
hidetomo-watanabe <attyamunn@gmail.com>
2019-03-30 23:50:09 +0000

    image sample追加

---
+++ modules/image_sample.py
@@ -0,0 +1,14 @@
+import numpy as np
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+
+
+def translate_image2feature_with_vgg16(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))
+    vgg16_model = VGG16(weights='imagenet', include_top=False)
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    features = vgg16_model.predict(x).flatten()
+    return features
