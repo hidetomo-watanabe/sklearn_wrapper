3d8c9203f548e88cf9f0a11335aa86cee2fd40fe
hidetomo-watanabe <attyamunn@gmail.com>
2019-08-20 02:49:29 +0000

    add vote into integrate

--- modules/Integrater.py
+++ modules/Integrater.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from itertools import combinations
 from IPython.display import display
@@ -28,9 +29,64 @@ class Integrater(ConfigReader):
         logger.info(f'mode: {mode}')
         if mode == 'average':
             return self._calc_average()
+        elif mode == 'vote':
+            return self._calc_vote()
         else:
             logger.error('NOT IMPLEMENTED INTEGRATE MODE: %s' % mode)
             raise Exception('NOT IMPLEMENTED')
+
+    def _calc_vote(self):
+        filenames = self.configs['integrate']['filenames']
+        weights = self.configs['integrate'].get('weights')
+        if not weights:
+            weights = [1] * len(self.filenames)
+        dfs = [pd.read_csv(filename) for filename in filenames]
+
+        def _get_id_df():
+            id_dfs = []
+            for df in dfs:
+                id_dfs.append(df[self.id_col])
+            id_df = id_dfs[0]
+            for id_df_tmp in id_dfs:
+                if not id_df.equals(id_df_tmp):
+                    logger.error('NOT SAME IDS OVER ALL FILES')
+                    raise Exception('DATA INCOSISTENCY')
+            return id_df
+
+        def _get_val_columns():
+            cols_list = []
+            for df in dfs:
+                cols_list.append(df.drop(self.id_col, axis=1).columns)
+            cols = cols_list[0]
+            for cols_tmp in cols_list:
+                if set(cols) != set(cols_tmp):
+                    logger.error('NOT SAME COLUMNS OVER ALL FILES')
+                    raise Exception('DATA INCOSISTENCY')
+            return cols
+
+        def _calc_results(val_column):
+            vals = []
+            for df, weight in zip(dfs, weights):
+                val_df = df[val_column]
+                vals.append(val_df.values.flatten())
+            vals = np.array(vals)
+            labels = list(set(vals.flatten()))
+
+            vote_table = np.array([
+                np.dot(
+                    np.array(weights).T,
+                    (np.vstack(vals) == i).astype(int)) for i in labels
+            ])
+            results = []
+            for i in np.argmax(vote_table, axis=0):
+                results.append(labels[i])
+            return results
+
+        self.output = pd.DataFrame()
+        self.output[self.id_col] = _get_id_df()
+        for val_column in _get_val_columns():
+            self.output[val_column] = _calc_results(val_column)
+        return self.output
 
     def _calc_average(self):
         filenames = self.configs['integrate']['filenames']
