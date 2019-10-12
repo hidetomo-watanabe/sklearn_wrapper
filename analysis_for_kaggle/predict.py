87c1f8e7eddaf2efc5b76ae291a69824d2f8fa53
hidetomo-watanabe <attyamunn@gmail.com>
2019-10-12 06:25:10 +0000

    predictでのメモリ消費表示

--- analysis_for_kaggle/predict.py
+++ analysis_for_kaggle/predict.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import traceback
+from memory_profiler import profile
 import logging.config
 from logging import getLogger
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -9,6 +10,62 @@ from modules.DataTranslater import DataTranslater
 from modules.Trainer import Trainer
 from modules.Outputer import Outputer
 from modules.Notifier import Notifier
+
+
+@profile
+def main(config_path):
+    # data translate
+    translater_obj = DataTranslater()
+    translater_obj.read_config_file(config_path)
+    translater_obj.get_translater()
+
+    logger.info('### DATA FOR VIEW')
+    translater_obj.create_data_for_view()
+    translater_obj.display_data()
+
+    logger.info('### TRANSLATE DATA FOR VIEW')
+    translater_obj.translate_data_for_view()
+    translater_obj.display_data()
+
+    logger.info('### WRITE DATA FOR VIEW')
+    translater_obj.write_data_for_view()
+
+    logger.info('### DATA FOR MODEL')
+    translater_obj.create_data_for_model()
+
+    logger.info('### TRANSLATE DATA FOR MODEL')
+    translater_obj.translate_data_for_model()
+
+    logger.info('### GET DATA FOR MODEL')
+    data_for_model = translater_obj.get_data_for_model()
+    post_processers = translater_obj.get_post_processers()
+
+    # train
+    trainer_obj = Trainer(**data_for_model)
+    trainer_obj.read_config_file(config_path)
+
+    logger.info('### FIT')
+    trainer_obj.calc_estimator()
+
+    logger.info('### WRITE ESTIMATOR DATA')
+    trainer_obj.write_estimator_data()
+
+    logger.info('### GET ESTIMATOR DATA')
+    estimator_data = trainer_obj.get_estimator_data()
+
+    # output
+    outputer_obj = Outputer(
+        **data_for_model, **estimator_data, **post_processers)
+    outputer_obj.read_config_file(config_path)
+
+    logger.info('### PREDICT')
+    outputer_obj.predict_y()
+    outputer_obj.calc_predict_df()
+
+    logger.info('### WRITE PREDICT DATA')
+    outputer_obj.write_predict_data()
+    return
+
 
 if __name__ == '__main__':
     logging.config.fileConfig(
@@ -22,58 +79,9 @@ if __name__ == '__main__':
         config_path = sys.argv[1]
     else:
         config_path = f'{BASE_PATH}/configs/config.json'
+
     try:
-        # data translate
-        translater_obj = DataTranslater()
-        translater_obj.read_config_file(config_path)
-        translater_obj.get_translater()
-
-        logger.info('### DATA FOR VIEW')
-        translater_obj.create_data_for_view()
-        translater_obj.display_data()
-
-        logger.info('### TRANSLATE DATA FOR VIEW')
-        translater_obj.translate_data_for_view()
-        translater_obj.display_data()
-
-        logger.info('### WRITE DATA FOR VIEW')
-        translater_obj.write_data_for_view()
-
-        logger.info('### DATA FOR MODEL')
-        translater_obj.create_data_for_model()
-
-        logger.info('### TRANSLATE DATA FOR MODEL')
-        translater_obj.translate_data_for_model()
-
-        logger.info('### GET DATA FOR MODEL')
-        data_for_model = translater_obj.get_data_for_model()
-        post_processers = translater_obj.get_post_processers()
-
-        # train
-        trainer_obj = Trainer(**data_for_model)
-        trainer_obj.read_config_file(config_path)
-
-        logger.info('### FIT')
-        trainer_obj.calc_estimator()
-
-        logger.info('### WRITE ESTIMATOR DATA')
-        trainer_obj.write_estimator_data()
-
-        logger.info('### GET ESTIMATOR DATA')
-        estimator_data = trainer_obj.get_estimator_data()
-
-        # output
-        outputer_obj = Outputer(
-            **data_for_model, **estimator_data, **post_processers)
-        outputer_obj.read_config_file(config_path)
-
-        logger.info('### PREDICT')
-        outputer_obj.predict_y()
-        outputer_obj.calc_predict_df()
-
-        logger.info('### WRITE PREDICT DATA')
-        outputer_obj.write_predict_data()
-
+        main(config_path)
     except Exception as e:
         logger.error('%s' % e)
         traceback.print_exc()
@@ -81,4 +89,5 @@ if __name__ == '__main__':
         notifier_obj = Notifier()
         notifier_obj.read_config_file(config_path)
         notifier_obj.notify_slack()
+
     logger.info('# FINISHED')
