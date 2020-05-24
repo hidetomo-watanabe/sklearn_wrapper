013e1fe16d558dc7b8597ed563e9bc010094b8ef
hidetomo-watanabe <attyamunn@gmail.com>
2020-05-24 15:13:22 +0900

    validation dataのaugmentation時にtrainに数を合わせる

--- analysis_for_kaggle/modules/trainers/Augmentor.py
+++ analysis_for_kaggle/modules/trainers/Augmentor.py
@@ -1,5 +1,7 @@
 from keras.preprocessing import image
 
+import numpy as np
+
 
 class Augmentor(object):
     def __init__(self, conf):
@@ -9,8 +11,20 @@ class Augmentor(object):
         self.datagen.fit(X)
         return self
 
-    def fit_resample(self, X, y):
+    def fit_resample(self, X, y, ratio=1):
         self.fit(X, y)
-        batch_itr = self.datagen.flow(X, y, batch_size=len(X), seed=42)
-        batch = next(batch_itr)
-        return batch[0], batch[1]
+        batch_itr = self.datagen.flow(
+            X, y, batch_size=len(X), seed=42)
+        new_X = None
+        new_y = None
+        for _ in range(ratio):
+            batch = next(batch_itr)
+            if isinstance(new_X, np.ndarray):
+                new_X = np.append(new_X, batch[0], axis=0)
+            else:
+                new_X = batch[0]
+            if isinstance(new_y, np.ndarray):
+                new_y = np.append(new_y, batch[1], axis=0)
+            else:
+                new_y = batch[1]
+        return new_X, new_y
