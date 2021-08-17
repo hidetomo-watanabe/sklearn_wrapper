8d0c51f4a1b89461312dabdc7994e1ce5ad4e70e
hidetomo-watanabe <attyamunn@gmail.com>
2021-08-17 14:50:33 +0900

    bugfix

--- sklearn_wrapper/modules/trainers/EnsembleTrainer.py
+++ sklearn_wrapper/modules/trainers/EnsembleTrainer.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import (
-    StackingClassifier,
+    StackingClassifier, StackingRegressor,
     VotingClassifier, VotingRegressor
 )
 from sklearn.metrics import get_scorer
